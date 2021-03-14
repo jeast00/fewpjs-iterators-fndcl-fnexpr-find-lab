3fba1b340d01439d612ac369ce1ec4beab38a840
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-03-14 09:14:22 -0400

    passed test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(record) {
+    let yearResult = record.find(r => r.result === 'W')
+    if(yearResult) {
+        return yearResult.year;
+    } else {
+        return undefined;
+    }
+}
